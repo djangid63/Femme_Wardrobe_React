715ed9cc12f9d84c275899b89e2828f3329cecf7
djangid63 <jangiddevesh031@gmail.com>
2025-01-22 22:54:47 +0530

    Started towards MPW

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,11 @@
 import React from "react";
-import HeroSection from "./components/HeroSection/HeroSection.jsx";
-import ProductCard1 from "./components/ProductSection/PopularProduct.jsx";
-import productData from "./Data/Data";
-import newProductData from "./Data/NewProduct.jsx";
-import BagSection from "./components/ProductSection/BagSection.jsx";
-import BlazerSection from './components/ProductSection/BlazerSection';
-import ReinventedSection from "./components/ProductSection/ReinventedSection.jsx";
-import NewProductCard from "./components/ProductSection/NewProductCard.jsx";
-import ReviewSection from "./components/ReviewSection/ReviewSection.jsx";
-import FeaturesSection from "./components/ReviewSection/featuresSection.jsx";
-import ExploreSection from "./components/ExploreSection/ExploreSection.jsx";
-import FooterSection from "./components/ExploreSection/FotterSection.jsx";
+
+import HomePages from "./Pages/HomePages.jsx";
 
 function App() {
   return (
     <div className="overflow-x-hidden">
-      <HeroSection />
-      <ProductCard1 productData={productData} />
-      <BagSection />
-      <BlazerSection />
-      <ReinventedSection />
-      <NewProductCard newProductData={newProductData} />
-      <ReviewSection />
-      <FeaturesSection />
-      <ExploreSection />
-      <FooterSection />
+      <HomePages />
     </div>
   );
 }
---
+++ src/Pages/HomePages.jsx
@@ -0,0 +1,32 @@
+import React from 'react'
+import HeroSection from "../components/HeroSection/HeroSection.jsx";
+import ProductCard1 from "../components/ProductSection/PopularProduct.jsx";
+import productData from "../Data/Data";
+import newProductData from "../Data/NewProduct.jsx";
+import BagSection from "../components/ProductSection/BagSection.jsx";
+import BlazerSection from '../components/ProductSection/BlazerSection';
+import ReinventedSection from "../components/ProductSection/ReinventedSection.jsx";
+import NewProductCard from "../components/ProductSection/NewProductCard.jsx";
+import ReviewSection from "../components/ReviewSection/ReviewSection.jsx";
+import FeaturesSection from "../components/ReviewSection/featuresSection.jsx";
+import ExploreSection from "../components/ExploreSection/ExploreSection.jsx";
+import FooterSection from "../components/ExploreSection/FotterSection.jsx";
+
+const HomePages = () => {
+  return (
+    <div>
+      <HeroSection />
+      <ProductCard1 productData={productData} />
+      <BagSection />
+      <BlazerSection />
+      <ReinventedSection />
+      <NewProductCard newProductData={newProductData} />
+      <ReviewSection />
+      <FeaturesSection />
+      <ExploreSection />
+      <FooterSection />
+    </div>
+  )
+}
+
+export default HomePages
