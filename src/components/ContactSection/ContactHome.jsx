b7028a20d00d358d16109ed121f8d76b842bd851
djangid63 <jangiddevesh031@gmail.com>
2025-02-08 23:08:53 +0530

    Update ContactHome component: adjust background image styling and modify NavBar colors; change heading to 'Contact Us'

--- src/components/ContactSection/ContactHome.jsx
+++ src/components/ContactSection/ContactHome.jsx
@@ -5,18 +5,14 @@ import ContactUsBgImg from '../../images/BgImages/ContactUsBgImg.jpeg'
 const ContactHome = () => {
   return (
     <div className='w-full h-[80vh] flex flex-col items-center justify-start relative'>
-      <img className='absolute inset-0 w-full h-[85vh] object-cover object-right-top' src={ContactUsBgImg} />
+      <img className='absolute inset-0 w-full h-[85vh] object-cover' src={ContactUsBgImg} />
       {/* Overlay */}
       <div className='absolute inset-0 w-full h-[85vh] bg-black opacity-50'></div>
       <div className='absolute'>
-        <NavBarCustom navBarTextColor={'black'} navBarBgColor={'white'} />
+        <NavBarCustom navBarTextColor={'white'} navBarBgColor={'transparent'} />
       </div>
       <div className='absolute top-[48%] mr-8 text-white '>
-        <h1 className='text-8xl font-bold font-mainHead mb-3'>Shop</h1>
-        <div className='flex items-end gap-2 ml-4'>
-          <span className='text-sm font-semibold font-mont'>Home -</span>
-          <span className='text-sm font-semibold font-mont'> - Shop</span>
-        </div>
+        <h1 className='text-7xl font-bold font-mainHead mb-3'>Contact Us</h1>
       </div>
     </div>
   )
